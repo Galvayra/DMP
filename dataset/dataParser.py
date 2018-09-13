513bc4f0c6f8b6f31a3c42e7e27d810506aeb8c1
nlp207-Server <galvayra@github.com>
2018-09-13 18:52:05 +0900

    have to make encoding

--- dataset/dataParser.py
+++ dataset/dataParser.py
@@ -1,4 +1,5 @@
 from DMP.dataset.dataHandler import DataHandler
+import math
 
 
 class DataParser(DataHandler):
@@ -18,12 +19,40 @@ class DataParser(DataHandler):
 
         for header, data_dict in self.x_data_dict.items():
             print(header)
-            for k, v in self.__get_data_dict(data_dict).items():
-                print(k, v)
+
+            self.__inspect_columns(data_dict)
+            # for data, count in self.__get_data_dict(data_dict).items():
+            #     print(data, count[0])
             print()
             print()
 
         self.free()
+
+    @staticmethod
+    def __inspect_columns(data_dict):
+        # show result of columns inspecting
+
+        type_dict = {"total": 0}
+        for _k, v in data_dict.items():
+
+            key = 0
+            if type(v) is float:
+                if math.isnan(v):
+                    key = "nan"
+                else:
+                    key = "float"
+            elif type(v) is str:
+                key = "str"
+            elif type(v) is int:
+                key = "int"
+
+            if key not in type_dict:
+                type_dict[key] = 1
+            else:
+                type_dict[key] += 1
+            type_dict["total"] += 1
+
+        print(type_dict)
 
     @staticmethod
     def __get_data_dict(data_dict):
@@ -32,8 +61,9 @@ class DataParser(DataHandler):
         for k, v in data_dict.items():
             # key = str(k) + "@" + str(v)
             if v not in new_data_dict:
-                new_data_dict[v] = 1
+                new_data_dict[v] = [1, [k]]
             else:
-                new_data_dict[v] += 1
+                new_data_dict[v][0] += 1
+                new_data_dict[v][1].append(k)
 
         return new_data_dict
