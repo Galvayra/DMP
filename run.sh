8bdb79f199463d35485d776e70861129b86df87c
nlp207-Server <voltexlicer@gmail.com>
2018-10-25 19:33:40 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #/bin/bash
-#cat *.txt > mergd
 
-./train.sh
-./predict.sh
+epoch=10000
+learn=0.00001
+hidden=2
+show=1
+plot=0
+dir=""
+model=ffnn
+vector=""
+result=""
+
+. utils/parse_options.sh || echo "Can't find parse_options.sh" | exit 1
+
+if [ "$vector" == "" ]; then
+    echo
+	echo "please input vector"
+	echo
+else
+	python training.py -vector "$vector"
+fi
