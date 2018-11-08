378c511173f0868e869e209d90b5818e01e3a451
nlp207-Server <voltexlicer@gmail.com>
2018-11-08 19:22:29 +0900

    experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -29,29 +29,29 @@
 #./test.sh --vector modeling/vectors/all_sampling --log cnn_h_4 --model cnn --epoch 10000
 
 
-#./run.sh --vector modeling/vectors/all_sampling --log ffnn_1 --model ffnn --epoch 10000 --learn 0.000002 --hidden 1 --result ffnn_1
-./run.sh --vector modeling/vectors/all_sampling --log ffnn_2 --model ffnn --epoch 10000 --learn 0.000002 --hidden 2 --result ffnn_2
-#./run.sh --vector modeling/vectors/all_sampling --log ffnn_3 --model ffnn --epoch 10000 --learn 0.000002 --hidden 3 --result ffnn_3
-#./run.sh --vector modeling/vectors/all_sampling --log ffnn_4 --model ffnn --epoch 10000 --learn 0.000002 --hidden 4 --result ffnn_4
-#./run.sh --vector modeling/vectors/all_sampling --log cnn_1 --model cnn --epoch 10000 --learn 0.000001 --hidden 1 --result cnn_1
-./run.sh --vector modeling/vectors/all_sampling --log cnn_2 --model cnn --epoch 10000 --learn 0.000001 --hidden 2 --result cnn_2
-#./run.sh --vector modeling/vectors/all_sampling --log cnn_3 --model cnn --epoch 10000 --learn 0.000001 --hidden 3 --result cnn_3
-#./run.sh --vector modeling/vectors/all_sampling --log cnn_4 --model cnn --epoch 10000 --learn 0.000001 --hidden 4 --result cnn_4
+./run.sh --vector modeling/vectors/all_sampling --log ffnn_1 --model ffnn --epoch 10000 --learn 0.000002 --hidden 1 --result ffnn_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log ffnn_2 --model ffnn --epoch 10000 --learn 0.000002 --hidden 2 --result ffnn_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log ffnn_3 --model ffnn --epoch 10000 --learn 0.000002 --hidden 3 --result ffnn_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log ffnn_4 --model ffnn --epoch 10000 --learn 0.000002 --hidden 4 --result ffnn_4 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log cnn_1 --model cnn --epoch 10000 --learn 0.000001 --hidden 1 --result cnn_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log cnn_2 --model cnn --epoch 10000 --learn 0.000001 --hidden 2 --result cnn_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log cnn_3 --model cnn --epoch 10000 --learn 0.000001 --hidden 3 --result cnn_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling --log cnn_4 --model cnn --epoch 10000 --learn 0.000001 --hidden 4 --result cnn_4 --delete 1
 
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_1 --model ffnn --epoch 10000 --learn 0.000002 --hidden 1 --result ffnn_w2v_1
-./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_2 --model ffnn --epoch 10000 --learn 0.000002 --hidden 2 --result ffnn_w2v_2
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_3 --model ffnn --epoch 10000 --learn 0.000002 --hidden 3 --result ffnn_w2v_3
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_4 --model ffnn --epoch 10000 --learn 0.000002 --hidden 4 --result ffnn_w2v_4
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_1 --model cnn --epoch 10000 --learn 0.000002 --hidden 1 --result cnn_w2v_1
-./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_2 --model cnn --epoch 10000 --learn 0.000002 --hidden 2 --result cnn_w2v_2
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_3 --model cnn --epoch 10000 --learn 0.000002 --hidden 3 --result cnn_w2v_3
-#./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_4 --model cnn --epoch 10000 --learn 0.000002 --hidden 4 --result cnn_w2v_4
+./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_1 --model ffnn --epoch 5000 --learn 0.000002 --hidden 1 --result ffnn_w2v_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_2 --model ffnn --epoch 5000 --learn 0.000002 --hidden 2 --result ffnn_w2v_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_3 --model ffnn --epoch 5000 --learn 0.000002 --hidden 3 --result ffnn_w2v_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log ffnn_w2v_4 --model ffnn --epoch 5000 --learn 0.000002 --hidden 4 --result ffnn_w2v_4 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_1 --model cnn --epoch 5000 --learn 0.000002 --hidden 1 --result cnn_w2v_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_2 --model cnn --epoch 5000 --learn 0.000002 --hidden 2 --result cnn_w2v_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_3 --model cnn --epoch 5000 --learn 0.000002 --hidden 3 --result cnn_w2v_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v --log cnn_w2v_4 --model cnn --epoch 5000 --learn 0.000002 --hidden 4 --result cnn_w2v_4 --delete 1
 
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_1 --model ffnn --epoch 10000 --learn 0.000002 --hidden 1 --result ffnn_w2v_scaling_1
-./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_2 --model ffnn --epoch 10000 --learn 0.000005 --hidden 2 --result ffnn_w2v_scaling_2
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_3 --model ffnn --epoch 10000 --learn 0.000002 --hidden 3 --result ffnn_w2v_scaling_3
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_4 --model ffnn --epoch 10000 --learn 0.000002 --hidden 4 --result ffnn_w2v_scaling_4
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_1 --model cnn --epoch 10000 --learn 0.000002 --hidden 1 --result cnn_w2v_scaling_1
-./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_2 --model cnn --epoch 10000 --learn 0.000005 --hidden 2 --result cnn_w2v_scaling_2
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_3 --model cnn --epoch 10000 --learn 0.000002 --hidden 3 --result cnn_w2v_scaling_3
-#./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_4 --model cnn --epoch 10000 --learn 0.000002 --hidden 4 --result cnn_w2v_scaling_4
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_1 --model ffnn --epoch 10000 --learn 0.000002 --hidden 1 --result ffnn_w2v_scaling_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_2 --model ffnn --epoch 10000 --learn 0.000002 --hidden 2 --result ffnn_w2v_scaling_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_3 --model ffnn --epoch 10000 --learn 0.000002 --hidden 3 --result ffnn_w2v_scaling_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log ffnn_w2v_scaling_4 --model ffnn --epoch 10000 --learn 0.000002 --hidden 4 --result ffnn_w2v_scaling_4 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_1 --model cnn --epoch 10000 --learn 0.000001 --hidden 1 --result cnn_w2v_scaling_1 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_2 --model cnn --epoch 10000 --learn 0.000001 --hidden 2 --result cnn_w2v_scaling_2 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_3 --model cnn --epoch 10000 --learn 0.000001 --hidden 3 --result cnn_w2v_scaling_3 --delete 1
+./run.sh --vector modeling/vectors/all_sampling_w2v_scaling --log cnn_w2v_scaling_4 --model cnn --epoch 10000 --learn 0.000001 --hidden 4 --result cnn_w2v_scaling_4 --delete 1
