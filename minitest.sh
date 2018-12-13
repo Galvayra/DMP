40922fac8921b04feb4994b2d34e826593591479
nlp207-Server <voltexlicer@gmail.com>
2018-12-13 16:43:09 +0900

    updated .gitignore

--- minitest.sh
+++ minitest.sh
@@ -1,24 +1,19 @@
 ## train ffnn
 #/bin/bash
 
-# w2v를 사용한 경우 loss 감소율이 2배로 늦음... (scaling 유무 상관 x)
-# w2v를 사용하지 않은 경우 FFNN --> 5000 epoch // lr 4e-06  // cnn --> 10000 epoch // lr 2e-06
-# 좀 더 epoch을 줄이고 lr을 올릴 필요가 있음
-
-
 # minimal test
 
 for vector in $@
 do
 	echo "test" $vector
 
-#	./run.sh --vector modeling/vectors/$vector --log ffnn_h1_$vector --model ffnn --epoch 2500 --learn 0.00001 --hidden 1 --result ffnn_h1_$vector --delete 1
-#	./run.sh --vector modeling/vectors/$vector --log ffnn_h2_$vector --model ffnn --epoch 2500 --learn 0.00001 --hidden 2 --result ffnn_h2_$vector --delete 1
-#	./run.sh --vector modeling/vectors/$vector --log ffnn_h3_$vector --model ffnn --epoch 2500 --learn 0.00001 --hidden 3 --result ffnn_h3_$vector --delete 1
-#	./run.sh --vector modeling/vectors/$vector --log ffnn_h4_$vector --model ffnn --epoch 2500 --learn 0.00001 --hidden 4 --result ffnn_h4_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log ffnn_h1_$vector --model ffnn --epoch 5000 --learn 0.00002 --hidden 1 --result ffnn_h1_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log ffnn_h2_$vector --model ffnn --epoch 5000 --learn 0.00002 --hidden 2 --result ffnn_h2_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log ffnn_h3_$vector --model ffnn --epoch 5000 --learn 0.00002 --hidden 3 --result ffnn_h3_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log ffnn_h4_$vector --model ffnn --epoch 5000 --learn 0.00002 --hidden 4 --result ffnn_h4_$vector --delete 1
 
-#	./run.sh --vector modeling/vectors/$vector --log cnn_h1_$vector --model cnn --epoch 5000 --learn 0.000005 --hidden 1 --result cnn_h1_$vector --delete 1
-    ./run.sh --vector modeling/vectors/$vector --log cnn_h2_$vector --model cnn --epoch 3000 --learn 0.000005 --hidden 2 --result cnn_h2_$vector --delete 1
-	./run.sh --vector modeling/vectors/$vector --log cnn_h3_$vector --model cnn --epoch 4000 --learn 0.000005 --hidden 3 --result cnn_h3_$vector --delete 1
-    ./run.sh --vector modeling/vectors/$vector --log cnn_h4_$vector --model cnn --epoch 4000 --learn 0.000005 --hidden 4 --result cnn_h4_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log cnn_h1_$vector --model cnn --epoch 5000 --learn 0.00002 --hidden 1 --result cnn_h1_$vector --delete 1
+    ./run.sh --vector modeling/vectors/$vector --log cnn_h2_$vector --model cnn --epoch 5000 --learn 0.00002 --hidden 2 --result cnn_h2_$vector --delete 1
+	./run.sh --vector modeling/vectors/$vector --log cnn_h3_$vector --model cnn --epoch 5000 --learn 0.00002 --hidden 3 --result cnn_h3_$vector --delete 1
+    ./run.sh --vector modeling/vectors/$vector --log cnn_h4_$vector --model cnn --epoch 5000 --learn 0.00002 --hidden 4 --result cnn_h4_$vector --delete 1
 done
