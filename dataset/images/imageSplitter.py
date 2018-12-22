46e8854545aef9db148f2dadf70cac4b6cc53faf
nlp207-Server <voltexlicer@gmail.com>
2018-12-22 14:59:22 +0900

    sampling

--- dataset/images/imageSplitter.py
+++ dataset/images/imageSplitter.py
@@ -3,22 +3,55 @@ from .variables import *
 from DMP.dataset.dataHandler import DataHandler
 import os
 import shutil
+import random
 
 
 class ImageSplitter(DataHandler):
     def __init__(self):
         super().__init__(read_csv=READ_CSV, data_path=DATA_PATH, do_what="splitting")
-        self.count()
-        self.make_train_test_dir()
+
+        # initialize train test directory
+        self.__init_train_test_dir()
+
+        # set dictionary of ct images
         self.__ct_dict = dict()
-        self.set_ct_dict()
+
+        # set patient info from csv file
+        self.__patient_number = [int(n) for n in self.x_data_dict[COLUMN_NUMBER].values()]
+        self.__patient_hospital = self.x_data_dict[COLUMN_HOSPITAL].values()
+        self.__patient_id = self.x_data_dict[COLUMN_ID].values()
+
+        # alive, death list (consist of patient number) = ['1', '2', ... 'n']
+        self.__alive_list = list()
+        self.__death_list = list()
+        self.__set_alive_death_list()
 
     @property
     def ct_dict(self):
         return self.__ct_dict
 
+    @property
+    def alive_list(self):
+        return self.__alive_list
+
+    @alive_list.setter
+    def alive_list(self, alive_list):
+        self.__alive_list = alive_list
+
+    @property
+    def death_list(self):
+        return self.__death_list
+
+    @property
+    def patient_number(self):
+        return self.__patient_number
+
+    @property
+    def patient_hospital(self):
+        return self.__patient_hospital
+
     @staticmethod
-    def make_train_test_dir():
+    def __init_train_test_dir():
         def __make_dir__(_path):
             if os.path.isdir(_path):
                 shutil.rmtree(_path)
@@ -29,14 +62,26 @@ class ImageSplitter(DataHandler):
         __make_dir__(DATA_PATH + TRAIN_DIR)
         __make_dir__(DATA_PATH + TEST_DIR)
 
-    def count(self):
-        cnt_mortality = self.counting_mortality(self.y_data)
-        cnt_total = len(self.y_data)
-        print("\n# of     total -", str(cnt_total).rjust(4),
-              "\n# of     alive -", str(cnt_total - cnt_mortality).rjust(4),
-              "\n# of mortality -", str(cnt_mortality).rjust(4), "\n\n")
+    def __show_count(self):
+        print("\n# of     total -", str(len(self.alive_list) + len(self.death_list)).rjust(4),
+              "\n# of     alive -", str(len(self.alive_list)).rjust(4),
+              "\n# of mortality -", str(len(self.death_list)).rjust(4), "\n\n")
+
+    def __set_alive_death_list(self):
+        for n, y in zip(self.patient_number, self.y_data):
+            if y == [0]:
+                self.alive_list.append(n)
+            else:
+                self.death_list.append(n)
+
+        self.__show_count()
+
+        # sampling alive using death ratio
+        self.alive_list = sorted(random.sample(self.alive_list, len(self.death_list)))
+
+        print("After applying the sampling!")
+        self.__show_count()
 
     def set_ct_dict(self):
-        
-        for i, n in self.x_data_dict[COLUMN_ID].items():
-            print(i, n)
+        pass
+
