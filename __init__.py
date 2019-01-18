85b05d3a74be0a40d21509387b1424d08e08021d
nlp207-Server <voltexlicer@gmail.com>
2019-01-18 17:52:07 +0900

    handling ct images

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from . import dataset, learning, modeling
 
-__all__ = ['encoding', 'parsing', 'training', 'predict', 'vector2txt']
+__all__ = ['encoding', 'parsing', 'training', 'predict', 'vector2txt', 'convert_images', 'build_ct_images',
+           'copy_ct_images']
