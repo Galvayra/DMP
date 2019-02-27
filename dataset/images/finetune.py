f6fbf17b13d5f51ddaff06d2674bc6580812ba1f
nlp207-Server <voltexlicer@gmail.com>
2019-02-27 17:31:31 +0900

    have to make fine tuning

--- dataset/images/finetune.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import os
-
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
-import tensorflow.contrib.slim.nets
-
-
-
---
+++ utils/arg_fine_tuning.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--train_dir', default='coco-animals/train')
+parser.add_argument('--val_dir', default='coco-animals/val')
+parser.add_argument('--model_path', default='vgg_16.ckpt', type=str)
+parser.add_argument('--batch_size', default=32, type=int)
+parser.add_argument('--num_workers', default=4, type=int)
+parser.add_argument('--num_epochs1', default=10, type=int)
+parser.add_argument('--num_epochs2', default=10, type=int)
+parser.add_argument('--learning_rate1', default=1e-3, type=float)
+parser.add_argument('--learning_rate2', default=1e-5, type=float)
+parser.add_argument('--dropout_keep_prob', default=0.5, type=float)
+parser.add_argument('--weight_decay', default=5e-4, type=float)
+
+args = parser.parse_args()
+
+VGG_MEAN = [123.68, 116.78, 103.94]
