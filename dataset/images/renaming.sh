9c0dcb18bc427b355dd80281d6cdfbcdf17c2bcb
Galvayra <voltexlicer@gmail.com>
2019-07-16 13:47:47 +0900

    renaming.sh for ct_images

--- dataset/images/renaming.sh
+++ dataset/images/renaming.sh
@@ -1,33 +1,50 @@
 #!/bin/sh
+folder="ct_images"
+cmd_dir=$(ls $folder | grep -v '[^0-9_]')
 
-cd 1
-echo "Dir '1'"
-echo
-./run.sh
-cd ..
+count() {
+	local sum=0
+	local cnt=0
+	
+	for dir in $cmd_dir
+	do
+		cd $folder/$dir
+		cnt=$(ls | wc -l)
+	
+		sum=$(($sum + $cnt))	
+		cd ../..
+	done
+	
+	echo "total  count = ${sum}"
+	echo
+}
 
-cd 2
-echo "Dir '2'"
-echo
-./run.sh
-cd ..
+rename() {
+	local cnt=0
+	local tmp="tmp"
 
-cd 3
-echo "Dir '3'"
-echo
-./run.sh
-cd ..
+	for dir in $cmd_dir
+	do
+		cd $folder/$dir
+		length=$(ls | wc -l)
+		
+		
+		for file in $(ls)
+		do
+			cnt=$(($cnt+1))
+			
+			mv $file $tmp
+			mv $tmp 00$cnt.jpg
+	
+		done
+	
+		cd ../..
+		cnt=0
+	done
+}
 
-cd 4
-echo "Dir '4'"
-echo
-./run.sh
-cd ..
 
-cd 5
-echo "Dir '5'"
-echo
-./run.sh
-cd ..
-
-
+count
+rename
+echo "Finish!"
+count
