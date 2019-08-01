a87e76a08c6134db129de257dfdc4fd9247a27e6
Galvayra <voltexlicer@gmail.com>
2019-08-01 11:29:41 +0900

    shell scripts for build

--- build_feature_selection.sh
+++ build_feature_selection.sh
@@ -6,13 +6,14 @@ vector="modeling/vectors/"
 fs_name=""
 ntree=""
 show=0
+softmax=0
 
 . utils/parse_options.sh || echo "Can't find parse_options.sh" | exit 1
 
 echo
 echo "################# vectorization of feature selection #################"
 echo
-python encoding.py -input "$input" -output "$output" -ver 2 -w2v 0
+python encoding.py -input "$input" -output "$output" -ver 2 -w2v 0 -softmax "$softmax"
 
 echo
 echo "################# get importance of features #################"
--- build_image_data.sh
+++ build_image_data.sh
@@ -9,8 +9,8 @@ export PYTHONPATH
 
 . utils/parse_options.sh || echo "Can't find parse_options.sh" | exit 1
 
-python dataset/images/parsing.py -output $input -sampling 0 -ratio 0.6
+python dataset/images/parsing.py -output "$input" -sampling 0 -ratio 0.6
 echo
 echo
-python dataset/images/encoding.py -input $input -output $output -w2v 0 -ver 1 -softmax $softmax
+python dataset/images/encoding.py -input "$input" -output "$output" -w2v 0 -ver 1 -softmax "$softmax"
 echo
