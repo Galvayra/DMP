e91cb316c80078492457c406bec282773a7b61bd
Galvayra <voltexlicer@gmail.com>
2019-08-03 09:27:44 +0900

    change the name and add parsing option

--- encoding_image.sh
+++ encoding_image.sh
@@ -4,14 +4,17 @@ base=""
 input="image"
 output="image_vector"
 softmax=0
+parsing=0
 
 PYTHONPATH=$PYTHONPATH:~/Project
 export PYTHONPATH
 
 . utils/parse_options.sh || echo "Can't find parse_options.sh" | exit 1
 
-python dataset/images/parsing.py -output "$input" -sampling 0 -ratio 0.6
-echo
+if [ $parsing -eq 1 ];then
+    python dataset/images/parsing.py -output "$input" -sampling 0 -ratio 0.6
+    echo
+fi
 echo
 python dataset/images/encoding.py -base "$base" -input "$input" -output "$output" -w2v 0 -ver 1 -softmax "$softmax"
 echo
