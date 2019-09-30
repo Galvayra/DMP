241dd220a683600a471d151fee9cffa7620b3d11
nlp207-Server <voltexlicer@gmail.com>
2019-09-30 21:10:48 +0900

    complete making transfer and fine-tuning

--- learning/slimLearner.py
+++ learning/slimLearner.py
@@ -34,7 +34,7 @@ class SlimLearner(TensorModel):
             shape.insert(0, None)
             self.shape = shape
 
-        self.early_stopping = EarlyStopping(patience=NUM_OF_EARLY_STOPPING, verbose=1, minimum_epoch=30)
+        self.early_stopping = EarlyStopping(patience=NUM_OF_EARLY_STOPPING, verbose=1, minimum_epoch=2)
 
         self.loss_dict = {
             "train": list(),
@@ -97,13 +97,13 @@ class SlimLearner(TensorModel):
     def __init_pre_trained_model(self):
         vgg = tf.contrib.slim.nets.vgg
         with slim.arg_scope(vgg.vgg_arg_scope()):
-            # If the model is transfer learning, parameter will be not trained during training
-            if self.model == "transfer":
-                is_training = False
-            # If the model is fine-tuning, parameter will be trained during training
-            else:
-                is_training = True
-            logits, end_points = vgg.vgg_16(inputs=self.tf_x, num_classes=1, is_training=is_training)
+            # # If the model is transfer learning, parameter will be not trained during training
+            # if self.model == "transfer":
+            #     is_training = False
+            # # If the model is fine-tuning, parameter will be trained during training
+            # else:
+            #     is_training = True
+            logits, end_points = vgg.vgg_16(inputs=self.tf_x, num_classes=1, is_training=False)
             # logits, end_points = vgg.vgg_16(inputs=self.tf_x, num_classes=1000, is_training=is_training)
 
             return logits, end_points
@@ -155,23 +155,50 @@ class SlimLearner(TensorModel):
         # return X*W + b
         return tf.add(tf.matmul(tf_layer[-1], tf_weight[-1]), tf_bias[-1])
 
+    # def __fine_tuning(self):
+    #     logits, end_points = self.__init_pre_trained_model()
+    #     str_n_fold = '_' + str(self.num_of_fold)
+    #     exclude = ['vgg_16/fc8']
+    #     variables_to_restore = slim.get_variables_to_restore(exclude=exclude)
+    #     # fc_7 = end_points['vgg_16/fc7']
+    #     fc_7 = tf.identity(end_points['vgg_16/fc7'], name="fc7" + str_n_fold)
+    #
+    #     W = tf.Variable(tf.random_normal([4096, 1], mean=0.0, stddev=0.02), name=NAME_FC_W + str_n_fold)
+    #     b = tf.Variable(tf.random_normal([1], mean=0.0), name=NAME_FC_B + str_n_fold)
+    #
+    #     fc = tf.reshape(fc_7, [-1, W.get_shape().as_list()[0]], name=NAME_FC + str_n_fold)
+    #     logitx = tf.nn.bias_add(tf.matmul(fc, W), b)
+    #
+    #     with tf.name_scope(NAME_SCOPE_COST):
+    #         hypothesis = tf.nn.sigmoid(logitx, name=NAME_HYPO + '_' + str(self.num_of_fold))
+    #         cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logitx, labels=self.tf_y))
+    #         cost_summary = tf.summary.scalar("cost", cost)
+    #
+    #     with tf.name_scope(NAME_SCOPE_PREDICT):
+    #         predict = tf.cast(hypothesis > 0.5, dtype=tf.float32, name=NAME_PREDICT + str_n_fold)
+    #         acc = tf.reduce_mean(tf.cast(tf.equal(predict, self.tf_y), dtype=tf.float32))
+    #         accuracy_summary = tf.summary.scalar("accuracy", acc)
+    #
+    #     if self.model == "transfer":
+    #         train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost, var_list=[W, b])
+    #     else:
+    #         train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost)
+    #
+    #     init_fn = slim.assign_from_checkpoint_fn(VGG_PATH, variables_to_restore)
+    #     self.__sess_run(hypothesis, train_step, cost, acc, init_fn)
+
     def __fine_tuning(self):
         logits, end_points = self.__init_pre_trained_model()
         exclude = ['vgg_16/fc8']
         variables_to_restore = slim.get_variables_to_restore(exclude=exclude)
-        fc_7 = end_points['vgg_16/fc7']
+        init_fn = slim.assign_from_checkpoint_fn(VGG_PATH, variables_to_restore)
 
         str_n_fold = '_' + str(self.num_of_fold)
-
-        W = tf.Variable(tf.random_normal([4096, 1], mean=0.0, stddev=0.02), name=NAME_FC_W + str_n_fold)
-        b = tf.Variable(tf.random_normal([1], mean=0.0), name=NAME_FC_B + str_n_fold)
-
-        fc = tf.reshape(fc_7, [-1, W.get_shape().as_list()[0]], name=NAME_FC + str_n_fold)
-        logitx = tf.nn.bias_add(tf.matmul(fc, W), b)
+        fc = tf.contrib.framework.get_variables('vgg_16/fc8')
 
         with tf.name_scope(NAME_SCOPE_COST):
-            hypothesis = tf.nn.sigmoid(logitx, name=NAME_HYPO + '_' + str(self.num_of_fold))
-            cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logitx, labels=self.tf_y))
+            hypothesis = tf.nn.sigmoid(logits, name=NAME_HYPO + str_n_fold)
+            cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=self.tf_y))
             cost_summary = tf.summary.scalar("cost", cost)
 
         with tf.name_scope(NAME_SCOPE_PREDICT):
@@ -180,35 +207,11 @@ class SlimLearner(TensorModel):
             accuracy_summary = tf.summary.scalar("accuracy", acc)
 
         if self.model == "transfer":
-            train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost, var_list=[W, b])
+            train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost, var_list=[fc])
         else:
             train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost)
 
-        init_fn = slim.assign_from_checkpoint_fn(VGG_PATH, variables_to_restore)
         self.__sess_run(hypothesis, train_step, cost, acc, init_fn)
-
-    # def __fine_tuning(self):
-    #     logits, end_points = self.__init_pre_trained_model()
-    #     exclude = ['vgg_16/fc8']
-    #     variables_to_restore = slim.get_variables_to_restore(exclude=exclude)
-    #     init_fn = slim.assign_from_checkpoint_fn(VGG_PATH, variables_to_restore)
-    #
-    #     with tf.name_scope(NAME_SCOPE_COST):
-    #         hypothesis = tf.nn.sigmoid(logits, name=NAME_HYPO + '_' + str(self.num_of_fold))
-    #         cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=self.tf_y))
-    #         cost_summary = tf.summary.scalar("cost", cost)
-    #
-    #     with tf.name_scope(NAME_SCOPE_PREDICT):
-    #         predict = tf.cast(hypothesis > 0.5, dtype=tf.float32, name=NAME_PREDICT + '_' + str(self.num_of_fold))
-    #         acc = tf.reduce_mean(tf.cast(tf.equal(predict, self.tf_y), dtype=tf.float32))
-    #         accuracy_summary = tf.summary.scalar("accuracy", acc)
-    #
-    #     if self.model == "transfer":
-    #         train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost)
-    #     else:
-    #         train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost)
-    #
-    #     self.__sess_run(hypothesis, train_step, cost, acc, init_fn)
 
     @staticmethod
     def __show_params(sess):
@@ -217,7 +220,7 @@ class SlimLearner(TensorModel):
         for k, v in zip(variables_names, values):
             print("Variable: ", k)
             print("Shape: ", v.shape)
-            # print(v)
+            print(v)
             print()
 
     def __training(self):
@@ -400,6 +403,13 @@ class SlimLearner(TensorModel):
                 else:
                     target = x_batch
 
+                # fc, h_batch = sess.run([self.fc, hypothesis],
+                #                        feed_dict={self.tf_x: target, self.tf_y: y_batch, self.keep_prob: 1})
+                #
+                # print(fc, fc.shape)
+                # print(h_batch, h_batch.shape)
+                # exit(-1)
+
                 h_batch = sess.run(hypothesis, feed_dict={self.tf_x: target, self.tf_y: y_batch, self.keep_prob: 1})
 
                 for h, y in zip(h_batch, y_batch):
@@ -432,22 +442,19 @@ class SlimLearner(TensorModel):
 
             # load tensor
             graph = tf.get_default_graph()
-            str_n_fold = str(self.num_of_fold)
+            str_n_fold = "_" + str(self.num_of_fold)
 
-            self.tf_x = graph.get_tensor_by_name(NAME_X + "_" + str_n_fold + ":0")
-            self.tf_y = graph.get_tensor_by_name(NAME_Y + "_" + str_n_fold + ":0")
-            self.keep_prob = graph.get_tensor_by_name(NAME_PROB + "_" + str_n_fold + ":0")
+            self.tf_x = graph.get_tensor_by_name(NAME_X + str_n_fold + ":0")
+            self.tf_y = graph.get_tensor_by_name(NAME_Y + str_n_fold + ":0")
+            self.keep_prob = graph.get_tensor_by_name(NAME_PROB + str_n_fold + ":0")
 
             # get num_of_hidden, learning_rate
-            num_of_hidden = graph.get_tensor_by_name(NAME_HIDDEN + "_" + str_n_fold + ":0")
-            learning_rate = graph.get_tensor_by_name(NAME_LEARNING_RATE + "_" + str_n_fold + ":0")
+            num_of_hidden = graph.get_tensor_by_name(NAME_HIDDEN + str_n_fold + ":0")
+            learning_rate = graph.get_tensor_by_name(NAME_LEARNING_RATE + str_n_fold + ":0")
             self.num_of_hidden, self.learning_rate = sess.run([num_of_hidden, learning_rate])
 
-            # self.w = graph.get_tensor_by_name(NAME_FC_W + '_' + str(self.num_of_fold) + ":0")
-            #     b = graph.get_tensor_by_name(NAME_FC_B + '_' + str(self.num_of_fold) +  ":0")
-
-            hypothesis = graph.get_tensor_by_name(NAME_SCOPE_COST + "/" + NAME_HYPO + "_" + str_n_fold + ":0")
-            # predict = graph.get_tensor_by_name(NAME_SCOPE_PREDICT + "/" + NAME_PREDICT + "_" + str_n_fold + ":0")
+            # self.fc = graph.get_tensor_by_name(NAME_FC + '_' + str(self.num_of_fold) + ":0")
+            hypothesis = graph.get_tensor_by_name(NAME_SCOPE_COST + "/" + NAME_HYPO + str_n_fold + ":0")
 
             self.__set_test_prob(sess, iterator_test, hypothesis)
 
