90da81baa83816907fde6d03eae82b605463eded
Galvayra <voltexlicer@gmail.com>
2019-12-30 12:43:41 +0900

    aa

--- dataset/images/variables.py
+++ dataset/images/variables.py
@@ -2,7 +2,7 @@
 
 IMAGE_CSV = "dataset_images.csv"
 CT_IMAGE_PATH = "ct_images/"
-CT_IMAGE_ALL_PATH = "all/"
+CT_IMAGE_ALL_PATH = "all2/"
 CT_IMAGE_TARGET_PATH = "target/"
 
 # directory path for ct images
